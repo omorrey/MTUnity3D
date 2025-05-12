71eff602592dd69c3387bd39df3ecd16dc5cf817
Nikita Kalygin <nstargaames@gmail.com>
2025-05-12 20:44:24 +0300

    Update Map, Health Bar enemy
    Changes in new Scene "SceneNifoxe"
    1) Map now bigger (Scale 3)
    2) Added Enemy with animations To Level
    3) Almost done Ranged Enemy
    4) Added healthbar

---
+++ Assets/ScriptsNifoxe/EnemyGun.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour
+{
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private Transform _pointSpawnBullet;
+
+    public void SpawnBullet()
+    {
+        Instantiate(_bulletPrefab, _pointSpawnBullet.position, transform.rotation);
+    }
+}
--- Assets/ScriptsNifoxe/EnemyHealth.cs
+++ Assets/ScriptsNifoxe/EnemyHealth.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int _health;
     [SerializeField] private GameObject _bonusGun;
+    [SerializeField] private Image _healthBar;
+
+    private int _maxHealth;
+
+    private void Start()
+    {
+        _maxHealth = _health;
+    }
     public void TakeDamage(int value)
     {
         _health -= value;
+        UpdateHealthBar();
 
         if (_health < 0)
         {
             Instantiate(_bonusGun, transform.position, Quaternion.Euler(90f, 0f, 0f));
             Destroy(gameObject);
         }
+    }
+
+    public void UpdateHealthBar()
+    {
+        _healthBar.fillAmount = (float)_health / _maxHealth;
     }
 }
